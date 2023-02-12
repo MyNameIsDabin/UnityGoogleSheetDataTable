89d412efa9d9cace63392aef138b20939387e952
Dabin <dorothy9795@gmail.com>
2023-02-12 22:27:36 +0900

    Update Sample.cs

--- GoogleSheetDataTable/Assets/Demo/Sample.cs
+++ GoogleSheetDataTable/Assets/Demo/Sample.cs
@@ -8,9 +8,6 @@ public class Sample : MonoBehaviour
     void Start()
     {
         // Access by Unique Key
-        var firstFood = FoodsTable.Instance.GetDataById(0); 
         
-        Debug.Log(firstFood.Name);
-        Debug.Log(firstFood.CanBuy);
     }
 }
